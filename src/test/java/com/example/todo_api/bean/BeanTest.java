15d7fd51f49d3253d42213c7139ecad1e22f495c
kckc0608 <kckc0608@naver.com>
2025-03-30 15:03:37 +0900

    feat: week2 빈 등록 방법 정리

---
+++ src/test/java/com/example/todo_api/bean/BeanTest.java
@@ -0,0 +1,32 @@
+package com.example.todo_api.bean;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanTest {
+
+    ApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    public void getAllBeanTest() {
+        // 스프링 컨테이너를 설정 파일 정보를 이용해서 생성하고, 스프링 컨테이너 안에 잇는 모든 빈을 조회하는 테스트
+        for (String name : context.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+
+        // context 안에 myBean 이 들어있는지 확인
+        Assertions.assertThat(context.getBeanDefinitionNames()).contains("myBean");
+    }
+
+    @Test
+    public void getOneBeanTest() {
+        MyBean bean1 = context.getBean(MyBean.class);
+        MyBean bean2 = context.getBean(MyBean.class);
+        MyBean bean3 = new MyBean();
+
+        Assertions.assertThat(bean1).isSameAs(bean2);
+        Assertions.assertThat(bean2).isNotEqualTo(bean3);
+    }
+}
